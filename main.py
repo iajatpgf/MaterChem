78054981b343251d9ad8afc4f6f126c5340b1245
iajatpgf <130942747+iajatpgf@users.noreply.github.com>
2024-04-01 17:30:08 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ with st.sidebar:
     openai_api_key = st.text_input("请输入OpenAI API Key：", type="password")
     st.markdown("[获取OpenAI API key](https://platform.openai.com/account/api-keys)")
     st.subheader('请选择所要使用的模型')
-    selected_model = st.sidebar.selectbox('选择一个模型', ['gpt-4-0125-preview', 'gpt-3.5-turbo', ''], key='selected_model')
+    "温馨提示，默认使用gpt-3.5模型，若想得到更好的回复，请选择gpt-4模型，但是更贵，约4分/字"
+    selected_model = st.sidebar.selectbox('选择一个模型', ['gpt-3.5-turbo', 'gpt-4-0125-preview'], key='selected_model')
 
 if "memory" not in st.session_state:
     st.session_state["memory"] = ConversationBufferMemory(return_messages=True)
@@ -26,7 +27,7 @@ for message in st.session_state["messages"]:
     if (len(st.session_state["messages"])) == 1:
         st.write("""你可以这样问我:
         
-        ‘你是一个广告公司的创意总监，帮我的产品写一段宣传语，产品是xxx，特点是xxx'
+        '你是一个广告公司的创意总监，帮我的产品写一段宣传语，产品是xxx，特点是xxx'
         
         '你是一个大学教授，帮我写一篇英文文章的摘要，主题是xxx'
         

edec3d72dbed08a66314d3fd9c3f8277b9d29e27
iajatpgf <130942747+iajatpgf@users.noreply.github.com>
2024-04-21 16:11:04 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -5,34 +5,38 @@ from utils import get_chat_response
 st.title("克隆ChatGPT")
 
 with st.sidebar:
+    link_html = """
+        <a href='https://testfile-quubpsyyqcdrvypetvw9zh.streamlit.app' target='_blank' style='color: red; font-size: 30px;'>📑文件分析工具</a>
+        """
+    st.markdown(link_html, unsafe_allow_html=True)
     openai_api_key = st.text_input("请输入OpenAI API Key：", type="password")
     st.markdown("[获取OpenAI API key](https://platform.openai.com/account/api-keys)")
     st.subheader('请选择所要使用的模型')
-    "温馨提示，默认使用gpt-3.5模型，若想得到更好的回复，请选择gpt-4模型，约0.4分/字"
-    selected_model = st.sidebar.selectbox('选择一个模型', ['gpt-3.5-turbo', 'gpt-4-turbo'], key='selected_model')
+    "默认使用gpt-3.5模型，若想得到更好的回复，请选择gpt-4模型"
+    selected_model = st.sidebar.selectbox('选择一个模型', ['gpt-3.5-turbo', 'gpt-4-turbo', 'gpt-4-0125-preview'], key='selected_model')
 
 if "memory" not in st.session_state:
     st.session_state["memory"] = ConversationBufferMemory(return_messages=True)
     st.session_state["messages"] = [{"role": "ai",
                                      "content": "你好，我是你的AI助手，有什么可以帮你的吗？"}]
 
-with st.sidebar:# 添加一个按钮，点击后重新设置会话状态
+with st.sidebar:  # 添加一个按钮，点击后重新设置会话状态
     if st.button('重新开始'):
         st.session_state["memory"] = ConversationBufferMemory(return_messages=True)
         st.session_state["messages"] = [{"role": "ai",
-                                     "content": "你好，我是你的AI助手，有什么可以帮你的吗？"}]
+                                         "content": "你好，我是你的AI助手，有什么可以帮你的吗？"}]
 
 for message in st.session_state["messages"]:
     st.chat_message(message["role"]).write(message["content"])
     if (len(st.session_state["messages"])) == 1:
         st.write("""你可以这样问我:
-        
+
         '你是一个广告公司的创意总监，帮我的产品写一段宣传语，产品是xxx，特点是xxx'
-        
+
         '你是一个大学教授，帮我写一篇英文文章的摘要，主题是xxx'
-        
+
         '扩写一下这个主题，300字左右，要求立意深刻’
-        
+
         ‘帮我润色以下这段内容，内容部分以3个#隔开，###here is your content###'""")
 
 prompt = st.chat_input()
